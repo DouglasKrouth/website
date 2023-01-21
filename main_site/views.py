3b992af128d886e0d4b013e5a10b6022639e034e
douglaskrouth <douglaskrouth@gmail.com>
2023-01-21 17:28:18 -0600

    misc updates for views

--- main_site/views.py
+++ main_site/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here.
+import datetime
 
-def home(request):
-    text = "<h1> rat </h1>"
-    return HttpResponse(text)
+def current_datetime(request):
+    now = datetime.datetime.now()
+    html = "<html><body>It is now %s.</body></html>" % now
+    return HttpResponse(html)
